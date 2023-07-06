7d7e327ac4124b51540a608c3def9829173eb9e2
Yuki Kondo <2023.04.y.kondo@gmail.com>
2023-07-07 08:43:13 +0900

    DAO

--- src/memo/MemoDAO.java
+++ src/memo/MemoDAO.java
@@ -13,8 +13,23 @@ public class MemoDAO {
 	}
 
 //	一件登録
-	public void insert(Map<String, String> productDTO) {
-		sqlSession.insert("product.insert", productDTO);
+	public void insert(Map<String, String> map) {
+		sqlSession.insert("product.insert", map);
+	}
+
+	public Map<String, String> writerfilter(String writer){
+//		selectList()メソッドの第1引数にはnamespace.idを、第二引数にはmapperに渡す変数を指定する
+		return sqlSession.selectMap("product.writerfilter", "%" + writer + "%");
+	}
+
+	public Map<String, String> memofilter(String memo){
+//		selectList()メソッドの第1引数にはnamespace.idを、第二引数にはmapperに渡す変数を指定する
+		return sqlSession.selectMap("product.memofilter", "%" + memo + "%");
+	}
+
+	public Map<String, String> filter(String memo ,String writer){
+//		selectList()メソッドの第1引数にはnamespace.idを、第二引数にはmapperに渡す変数を指定する
+		return sqlSession.selectMap("product.writerlist", "%" + memo + "%");
 	}
 
 //	詳細取得
@@ -23,8 +38,8 @@ public class MemoDAO {
 	}
 
 //	1件更新
-	public void update(Map<String, String> productDTO) {
-		sqlSession.update("product.update", productDTO);
+	public void update(Map<String, String> map) {
+		sqlSession.update("product.update", map);
 	}
 
 //	一件削除
@@ -32,8 +47,4 @@ public class MemoDAO {
 		sqlSession.delete("product.delete", idx);
 	}
 
-//	1件取得
-	public String filename(int idx) {
-		return sqlSession.selectOne("product.filename", idx);
-	}
 }
