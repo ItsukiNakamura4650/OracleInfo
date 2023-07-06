3de557e5ce3100363ae4488e635f3068617d8811
Yuki Kondo <2023.04.y.kondo@gmail.com>
2023-07-06 16:38:23 +0900

    コントローラーとDAO

--- src/memo/MemoDAO.java
+++ src/memo/MemoDAO.java
@@ -9,6 +9,31 @@ public class MemoDAO {
 
 	public Map<String, String> list(){
 //		selectList()メソッドの第1引数にはnamespace.idを、第二引数にはmapperに渡す変数を指定する
-		return sqlSession.selectList("product.list","");
+		return sqlSession.selectMap("product.select","");
+	}
+
+//	一件登録
+	public void insert(ProductDTO productDTO) {
+		sqlSession.insert("product.insert", productDTO);
+	}
+
+//	詳細取得
+	public ProductDTO detail(String product_code) {
+		return sqlSession.selectOne("product.detail", product_code);
+	}
+
+//	1件更新
+	public void update(ProductDTO productDTO) {
+		sqlSession.update("product.update", productDTO);
+	}
+
+//	一件削除
+	public void delete(int idx) {
+		sqlSession.delete("product.delete", idx);
+	}
+
+//	1件取得
+	public String filename(int idx) {
+		return sqlSession.selectOne("product.filename", idx);
 	}
 }
